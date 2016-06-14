fb1855dd9bfad5e264ca21702d41bc8d31928924
shacharz <shachar@peer5.com>
2016-06-14 15:15:56 +0300

    bundling all the videojs plugins together

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+var sources = ['src/**/*.js',
+    "node_modules/videojs-contrib-media-sources/dist/videojs-contrib-media-sources.min.js",
+    "node_modules/videojs-contrib-hls/dist/videojs-contrib-hls.min.js",
+    "node_modules/hls.js/dist/hls.min.js"];
+
+gulp.task('build', function() {
+    return gulp.src(sources)
+        .pipe(concat('videojs-contrib-hlsjs.js'))
+        .pipe(gulp.dest('dist/'));
+});
